f7eeb928796c4c8045708ca743fd074828ba8e31
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-01-22 01:46:46 +0100

    Input Form Template rendering implemented. Created Project and Project List classes based on their interfaces. Created submit form event which creates new Project class instance and push it to existing Project List instnace property project_list

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-function implementMethods(constructor) {
-    console.log(constructor);
-    return class extends constructor {
-        constructor(...rest) {
-            super();
-            console.log(constructor);
-        }
+const app_container = document.getElementById("app"); // app container 
+//TEMPLATES
+const project_input_template = document.getElementById("project-input");
+const single_project_template = document.getElementById("single-project");
+const project_list_template = document.getElementById("project-list");
+class Project_List {
+    constructor() {
+        this.projectList = [];
+    }
+    get showProjects() {
+        console.log(this.projectList);
+        return this.projectList;
+    }
+    addProjectToList(project) {
+        this.projectList.push(project);
+    }
+}
+class Project {
+    constructor(title, description, people_number) {
+        this.title = title;
+        this.description = description;
+        this.people_number = people_number;
+    }
+}
+// Inicjalizacja project input Template
+function initProjectInputTemplate() {
+    let project_input_template_clone = project_input_template.content.cloneNode(true);
+    app_container.appendChild(project_input_template_clone);
+    const project_input_title = document.getElementById("title");
+    const project_input_description = document.getElementById("description");
+    const project_input_people_number = document.getElementById("people");
+    const project_input_form = document.getElementById("project_input_form");
+    const project_list = new Project_List(); //Instancja klasy Project_List służąca nam do zawierania
+    //w niej nowych projektów wprowadzonych przez użytkownika.
+    return {
+        project_list: project_list,
+        title_input: project_input_title,
+        description_input: project_input_description,
+        people_input: project_input_people_number,
+        input_form: project_input_form
     };
 }
-let Calculate = class Calculate {
-    constructor(...rest) {
-        this.numberStorage = [];
-        this.numberStorage = rest;
-    }
-    showNumbers() {
-        console.log(this.numberStorage);
-    }
+//Wywołanie inicjalizacji
+const { project_list, title_input, description_input, people_input, input_form } = initProjectInputTemplate();
+//KOD
+//EventListener dla przycisku zatwierdzania formy
+const input_form_submit = (e, title_input, description_input, people_input) => {
+    e.preventDefault();
+    const project = new Project(title_input.value, description_input.value, +people_input.value);
+    project_list === null || project_list === void 0 ? void 0 : project_list.addProjectToList(project);
+    project_list === null || project_list === void 0 ? void 0 : project_list.showProjects;
 };
-Calculate = __decorate([
-    implementMethods
-], Calculate);
+input_form === null || input_form === void 0 ? void 0 : input_form.addEventListener("submit", (e) => input_form_submit(e, title_input, description_input, people_input));
--- app.ts
+++ app.ts
@@ -1,24 +1,87 @@
+const app_container = document.getElementById("app") as HTMLDivElement // app container 
 
-function implementMethods<T extends {new(...rest:any[]): {numberStorage:number[]}}>(constructor:T) {
-    console.log(constructor)
-    return class extends constructor {
-        constructor(...rest:any[]) {
-            super();
-            console.log(constructor)
+//TEMPLATES
+const project_input_template = document.getElementById("project-input") as HTMLTemplateElement
+const single_project_template = document.getElementById("single-project") as HTMLTemplateElement
+const project_list_template = document.getElementById("project-list") as HTMLTemplateElement
+
+interface Project_inputs {
+    project_list?: Project_List
+    title_input?: HTMLInputElement
+    description_input?: HTMLInputElement
+    people_input?: HTMLInputElement
+    input_form?: HTMLFormElement
+}
+
+interface Project_Interface {
+    title:string;
+    description:string;
+    people_number:number
+}
+
+class Project_List { //Obiekt listy projektów. Do niego dodawane sa poszczególne nowe instancje klasy Project.
+    projectList:Project_Interface[]= []
+    constructor() {
+    }
+
+    get showProjects() {
+        console.log(this.projectList);
+        return this.projectList
+    }
+
+    addProjectToList(project:Project_Interface) {
+        this.projectList.push(project)
+    } 
+}
+
+
+class Project implements Project_Interface{
+    constructor(
+        public title:string, 
+        public description:string, 
+        public people_number: number
+    ){}
+}
+
+
+// Inicjalizacja project input Template
+function initProjectInputTemplate():Project_inputs {
+        let project_input_template_clone = project_input_template.content.cloneNode(true)
+        app_container.appendChild(project_input_template_clone)
+
+        const project_input_title = document.getElementById("title") as HTMLInputElement
+        const project_input_description = document.getElementById("description") as HTMLInputElement
+        const project_input_people_number = document.getElementById("people") as HTMLInputElement
+        const project_input_form = document.getElementById("project_input_form") as HTMLFormElement
+        const project_list = new Project_List() //Instancja klasy Project_List służąca nam do zawierania
+            //w niej nowych projektów wprowadzonych przez użytkownika.
+        return {
+            project_list: project_list,
+            title_input: project_input_title,
+            description_input: project_input_description,
+            people_input: project_input_people_number,
+            input_form: project_input_form
         }
-
-    }
-    
-}
-
-@implementMethods
-class Calculate {
-    numberStorage:number[] = []
-    constructor(...rest:number[]) {
-        this.numberStorage=rest
     }
 
-    showNumbers() {
-        console.log(this.numberStorage)
+//Wywołanie inicjalizacji
+const {project_list, title_input, description_input, people_input, input_form}:Project_inputs = initProjectInputTemplate()
+
+
+//KOD
+
+//EventListener dla przycisku zatwierdzania formy
+
+const input_form_submit = (
+    e:any,
+    title_input:HTMLInputElement, 
+    description_input: HTMLInputElement, 
+    people_input:HTMLInputElement
+    ) => {
+        e.preventDefault()
+        const project = new Project(title_input.value, description_input.value, +people_input.value)
+        project_list?.addProjectToList(project)
+        project_list?.showProjects
     }
-}
+
+input_form?.addEventListener("submit", (e) => input_form_submit(e, title_input!, description_input!, people_input!)) 
